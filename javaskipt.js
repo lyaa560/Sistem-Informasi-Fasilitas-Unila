20c75d0fc9394e4e18fc1164c038e6b11b1cf30c
lyaa560 <alyaazahra908@gmail.com>
2024-12-13 13:55:25 +0700

    Add files via upload

---
+++ javaskipt.js
@@ -0,0 +1,59 @@
+// Menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+});
+
+// Check lebar
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+        // display: block
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
+// Efek scroll
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 0) {
+            $("nav").addClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+        } else {
+            $("nav").removeClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+        }
+    });
+
+    // button scroll
+    $(".tombol").click(function (e) {
+        e.preventDefault(); 
+        $("html, body").animate(
+            {
+                scrollTop: $("#aboutus").offset().top,
+            },
+            800 // duration
+        );
+    });
+});
